8a23236ec81bc25804473044e688a24e9985422b
DovudMadobidovOpensourceDeveloper <c38554372@gmail.com>
2025-01-22 22:54:52 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame
+
+def draw_rect(screen, color, x, y, width, height):
+	pygame.draw.rect(screen, color, (x, y, width, height))
+
+pygame.init()
+
+sc = pygame.display.set_mode((600, 400))
+sc.fill((33,33,33))
+pygame.display.flip()
+
+x = 10
+y = 10
+rect_width = 100
+rect_height = 100
+clock = pygame.time.Clock()
+
+
+# Устанавливаем параметры текста
+font = pygame.font.Font(None, 74)  # Используем шрифт по умолчанию
+text = font.render('Привет, Pygame!', True, (171,219,227))  # Белый цвет текста
+
+while True:
+	for ev in pygame.event.get():
+		if ev.type == pygame.QUIT:
+			pygame.quit()
+			exit()
+
+
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_DOWN] and y + rect_height < 400:
+		y+=10
+		print(y)
+	if keys[pygame.K_UP] and y > 0:
+		y-=10
+		print(y)
+	if keys[pygame.K_LEFT] and x > 0:
+		x-=10
+		print(x)
+	if keys[pygame.K_RIGHT] and x + rect_width < 600:
+		x+=10
+		print(x)
+	sc.fill((33, 33, 33))
+	draw_rect(sc, (0,0,255), x, y, 100, 100)
+	sc.blit(text, (100, 200))  # Позиция текста
+	pygame.display.flip()
+	clock.tick(60)
+	print(clock.get_fps())
